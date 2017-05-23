1279855168150e7a09c2903dbafd4717005abd16
wz <wzedan@gmail.com>
2017-05-23 09:03:27 +0300

    InitialCommit

---
+++ src/main/java/learning/io/designit/DesignItApplication.java
@@ -0,0 +1,12 @@
+package learning.io.designit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignItApplication.class, args);
+	}
+}
---
+++ src/test/java/learning/io/designit/DesignItApplicationTests.java
@@ -0,0 +1,16 @@
+package learning.io.designit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesignItApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
